3f0e6a7b1d34a2a98dde695ad6a650da11dc8ed5
safi islam <safiislam04@gmail.com>
2025-07-02 12:10:03 +0600

    add

--- src/app/page.js
+++ src/app/page.js
@@ -33,24 +33,21 @@ export default function Home() {
       <p className="text-xl text-center">Blogs</p>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 rounded-md">
         {blogs.map((i) => {
-          const blog = i.attributes;
-          const imageUrl = blog?.image?.data?.attributes?.url;
-
           return (
             <Link
-              href={`/blog/${blog.slug}`}
+              href={`/blog/${i.slug}`}
               key={i.id}
               className="border rounded-md p-4 flex flex-col items-center hover:shadow transition"
             >
-              {imageUrl && (
+              {i?.image?.url && (
                 <img
-                  src={`http://localhost:1337${imageUrl}`}
-                  alt={blog.title}
+                  src={`http://localhost:1337${i?.image?.url}`}
+                  alt={i.title}
                   className="w-full h-40 object-cover rounded"
                 />
               )}
-              <h1 className="text-lg font-bold mt-2">{blog.title}</h1>
-              <p className="text-sm text-gray-600">{blog.description}</p>
+              <h1 className="text-lg font-bold mt-2">{i.title}</h1>
+              <p className="text-sm text-gray-600">{i.description}</p>
             </Link>
           );
         })}
